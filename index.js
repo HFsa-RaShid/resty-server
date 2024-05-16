c2f93971ba9f73a20c757f7b7936210426335942
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-05-16 20:22:46 +0600

    jjjj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
     require('dotenv').config();
     const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
     const app = express();
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 8000;
     // middleWare
     app.use(cors());
     app.use(express.json())
